da6d0081ef1dd4a230d8606091822d6f6c63d525
fanyf-c <1078246618@qq.com>
2020-02-06 16:02:53 +0800

    第一次提交

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Created by yufeng on 2020/2/6.
+ */
+public class Test {
+    public static void main(String[] args) {
+        int a =1;
+        int b =2;
+        System.out.println(a+b);
+    }
+}
---
+++ src/Test2.java
@@ -0,0 +1,6 @@
+/**
+ * Created by yufeng on 2020/2/6.
+ */
+public class Test2 {
+
+}
